8e4c29c704fc532c0ed8d8827d0e7a34e291d0d7
Kaiei Kubota <shell.way.228.2@gmail.com>
2018-09-06 15:05:20 +0900

    JankenView.java  GUIの実装
    グー・チョキ・パーボタン、勝敗表示用ラベル、及びコメントエリアを作成するメソッドmyLayoutを作成した。

--- src/janken/JankenController.java
+++ src/janken/JankenController.java
@@ -8,13 +8,17 @@ public class JankenController {
 
      private final Random random = new Random();
      private  static final String[] hands = {
-         "グー", "チョキ", "パー"
-     };
+             "グー", "チョキ", "パー"
+         };
 
-      private Label cpuHandLabel;
-      private Label myHandLabel;
-      private Label resultLabel;
-      private Label scoreLabel;
+          private int win = 0;
+          private int draw = 0;
+          private int lose = 0;
+
+          private Label cpuHandLabel;
+          private Label myHandLabel;
+          private Label resultLabel;
+          private Label scoreLabel;
       //コンストラクタ
       //引数  3つのラベル
       public JankenController(Label cpuHandLabel, Label myHandLabel,
@@ -50,9 +54,17 @@ public class JankenController {
      private int getCpuHand() {
          return random.nextInt(3);
      }
-     //じゃんけんの結果の取得
+     //じゃんけんの結果のお取得
      private String getResult(int myHand, int cpuHand) {
          int r = (myHand - cpuHand + 3) % 3;
+
+         if(r == 2) {
+           win++;
+         } else if(r==1) {
+           lose++;
+         } else {
+           draw++;
+         }
 
          return (r == 2) ? "あなたの勝ち！" : ((r == 1) ? "あなたの負け！" : "あいこ！");
      }
@@ -61,5 +73,6 @@ public class JankenController {
          cpuHandLabel.setText("コンピュータの手: " + hands[cpuHand]);
          myHandLabel.setText("あなたの手: " + hands[myHand]);
          resultLabel.setText("結果: " + getResult(myHand, cpuHand));
+         scoreLabel.setText(" Win:"+win+" Draw:"+draw+" Lose:"+lose);
      }
 }
